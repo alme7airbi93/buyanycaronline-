578d398fd444dbad99a1428d065ecccc6541ec88
Dipesh Wagle <dipeshwagle@icloud.com>
2018-12-31 23:27:01 +0545

    enable cors

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import compression from "compression";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-// import cors from "cors";
+import cors from "cors";
 import expressValidator from "express-validator";
 import bluebird from "bluebird";
 import { MONGODB_URI } from "./util/secrets";
@@ -34,7 +34,7 @@ app.set("port", process.env.PORT || 3000);
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(cors());
 app.use(expressValidator());
 
 app.use("/api", ApiRoute);
