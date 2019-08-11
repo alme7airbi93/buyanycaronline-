47ff9ebeaecb5c231abb13707533cd534fa9fb95
alme7airbi <alme7airbi93@live.com>
2019-08-12 02:52:17 +0400

    new v

---
+++ frontend/src/app/modules/ad.model.ts
@@ -0,0 +1,12 @@
+export class AdModel {
+    id          : string;
+    user_id     : string;
+    title       : string;
+    description : string;
+    price       : number;
+    city        : string;
+    no          : number;
+    approve     : number;
+    publish     : boolean;
+    visitcount  : number;
+}
